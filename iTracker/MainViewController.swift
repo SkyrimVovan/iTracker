18749db5cc5a7e447711f355ad146aaec370e725
vova.petrosyuk <vladimir@Vladimirs-Mac.local>
2020-01-13 00:31:30 +0300

    Second commit with modified MainVC without username variable

--- iTracker/MainViewController.swift
+++ iTracker/MainViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class MainViewController: UIViewController {
     
-    //var username = String()
     var oneActive = false
     var whenTapped = Date()
     var indexPRow = IndexPath()
